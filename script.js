9a5fd9442e7112846e2d794fde30e06a873e9040
Maarten <maarten.postma1000@gmail.com>
2023-04-05 21:56:51 +0200

    add the tiger animation next to the button

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ const elementsArray = document.querySelectorAll(".not-active");
 elementsArray.forEach(function(elem) {
     elem.addEventListener("mouseover", function() {
         //this function does stuff
-        console.log(elementsArray)
         elem.classList.add("animate");
     });
+});
+
+const  textCta = document.querySelector(".cta-text");
+const btnCta = document.querySelector(".cta button");
+
+btnCta.addEventListener("mouseover", function() {
+
+    textCta.classList.add("animate-text-cta")
+});
+
+btnCta.addEventListener("mouseout", function() {
+
+    textCta.classList.remove("animate-text-cta")
 });

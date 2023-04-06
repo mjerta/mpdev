ea86ecebae30fa0d726ccf7d51ff6313df89d277
Maarten <maarten.postma1000@gmail.com>
2023-04-06 23:44:58 +0200

    add the tiger animation next to the button

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
+// nav section
 const elementsArray = document.querySelectorAll(".not-active");
 
-elementsArray.forEach(function(elem) {
-    elem.addEventListener("mouseover", function() {
-        //this function does stuff
-        elem.classList.add("animate");
-    });
+elementsArray.forEach(function (elem) {
+  elem.addEventListener("mouseover", function () {
+    elem.classList.add("animate");
+  });
 });
+//
 
-const  textCta = document.querySelector(".cta-text");
+// cta section
+const textCta = document.querySelector(".cta-text");
 const btnCta = document.querySelector(".cta button");
+const tigerCta = window.getComputedStyle(btnCta, "::before");
+console.log(tigerCta);
 
-btnCta.addEventListener("mouseover", function() {
-
-    textCta.classList.add("animate-text-cta")
+btnCta.addEventListener("mouseover", function () {
+  textCta.classList.add("animate-text-cta");
 });
 
-btnCta.addEventListener("mouseout", function() {
-
-    // textCta.classList.remove("animate-text-cta")
+btnCta.addEventListener("mouseout", function () {
+  // textCta.classList.remove("animate-text-cta")
+  //   tigerCta.visibility = "hidden";
+  btnCta.style.setProperty("--afterBack", "hidden");
 });
+
+//

d235347ba20a6051b639e20f5d18511c2f660841
Maarten <maarten.postma1000@gmail.com>
2023-04-13 07:06:21 +0200

    [4] enabling dark mode added a selector for the element hero-image-behind

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const textCta = document.querySelector(".cta-text");
 let btnCta = document.querySelector(".cta-btn-tiger");
 const hideElement = document.getElementById("hide-element");
 const leavesElement = document.getElementById("leaves");
+const heroImageBehind = document.querySelector(".hero-image-behind");
 // let tigerCta = window.getComputedStyle(btnCta, "::before");
 let count = 0;
 

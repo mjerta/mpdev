273668b79bf16e181f7421d6fa8b7cbfed5821a3
Maarten <maarten.postma1000@gmail.com>
2023-07-13 06:31:57 +0200

    Added comments & loop in js, class & trans. in css
    Added some comments to describe the inside for loops
    Added the second for loop to remove the card-opacity for the ones that are hovered over.
    Added the opacity class and added a style for the transition to the card class for it

--- script.js
+++ script.js
@@ -70,13 +70,19 @@ heroImageSun.addEventListener("click", function () {
 
 const addHoverStateCards = () => {
   const cardElements = document.querySelectorAll(".card");
+
+  // The loop below will add an eventlistener for each
+  // of the card available in the projects
   cardElements.forEach((cardElement) => {
     const cardTitleElement = cardElement.querySelector(".card h2");
     const cardSvgElement = cardElement.querySelector(".card h2 svg");
     const cardListElements = cardElement.querySelectorAll(".card li");
     cardElement.addEventListener("mouseover", () => {
+      const check = cardElement.classList.contains("card-hover");
       cardElement.classList.add("card-hover");
       // for loop to check which card is not been hovered over
+      // this for loop is need to be able to show which one is not selected
+      // while the one that is hovered over
       for (const checkCardElement of cardElements) {
         const check = checkCardElement.classList.contains("card-hover");
         if (!check) {
@@ -90,9 +96,18 @@ const addHoverStateCards = () => {
       }
     });
     cardElement.addEventListener("mouseout", () => {
+      //this for loop will remove the card-opacity class
+      // for the ones that was not hovered over
+      // this time it has been executed before the card-hover classlist remove
+      for (const checkCardElement of cardElements) {
+        const check = checkCardElement.classList.contains("card-hover");
+        if (!check) {
+          checkCardElement.classList.remove("card-opacity");
+        }
+      }
+      cardElement.classList.remove("card-hover");
       cardTitleElement.classList.remove("h2-hover");
       cardSvgElement.classList.remove("svg-hover");
-      cardElement.classList.remove("card-hover");
       for (const cardListElement of cardListElements) {
         cardListElement.classList.remove("li-hover");
       }

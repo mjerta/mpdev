b7cf4d691de243ce4333c3a8c2c5fd3c63b69e6b
mjerta <maarten.postma1000@gmail.com>
2024-03-03 14:07:13 +0100

    change token

--- api-github.js
+++ api-github.js
@@ -2,7 +2,7 @@ async function fetchGetGithubRepos() {
   const owner = "mjerta";
   const apiUrl = `https://api.github.com/users/mjerta/repos`;
   const token =
-    "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";
+    "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";
   const customHeaders = {
     Accept: "application/json",
     Authorization: `Bearer ${token}`,

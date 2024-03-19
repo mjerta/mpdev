bbec8e53b5d532040da1e3a4a67d2912155b89a1
mjerta <maarten.postma1000@gmail.com>
2024-03-19 06:51:14 +0100

    in progress

--- attribute-collector.js
+++ attribute-collector.js
@@ -8,3 +8,11 @@ function getDataAttributes(actionAttribute) {
     }
   }
 }
+
+function createExcert(text, maxLength) {
+  if (text.length <= maxLength) {
+    return text;
+  } else {
+    return text.substring(0, maxLength) + "...";
+  }
+}
